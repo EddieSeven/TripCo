9a0a92560415963300451a4c24a1ea5ab9baf9e4
Michael <mlyonsru@gmail.com>
2017-09-05 15:12:46 -0600

    rough draft of JSON writer

--- src/main/java/edu/csu2017fa314/T25/View/View.java
+++ src/main/java/edu/csu2017fa314/T25/View/View.java
@@ -1,6 +1,15 @@
 package edu.csu2017fa314.T25.View;
+
+import com.google.gson.Gson;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class View
 {
+   ArrayList<String> computedDistances = new ArrayList<String>(); /*[TODO] Replace with actual data structure*/
+
    private int totalDistance;
 
    public void setTotalDistance(int distance) 
@@ -11,6 +20,16 @@ public class View
    public int getTotalDistance() 
    {
       return totalDistance;
+   }
+
+   public void writeJSON(ArrayList<String> compDistances /*[TODO] Replace with actual data structure*/) throws IOException {
+      Gson gsonObj = new Gson();
+
+
+      FileWriter writer = new FileWriter("itinerary.json");
+      gsonObj.toJson(computedDistances, writer);
+
+
    }
 
 }
