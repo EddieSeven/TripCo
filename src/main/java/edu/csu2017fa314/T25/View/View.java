3c53423bfc82b5d15dfdab385799c9cb096c8109
Michael <mlyonsru@gmail.com>
2017-09-05 15:55:03 -0600

    Fixed writer, added some comments explaining what the function does.

--- src/main/java/edu/csu2017fa314/T25/View/View.java
+++ src/main/java/edu/csu2017fa314/T25/View/View.java
@@ -22,10 +22,16 @@ public class View
       return totalDistance;
    }
 
+
+   // Takes in a data structure consisting of a collection of data with three fields: id for one point, id for another point, and the distance between the two.
+   // Uses the gson class to turn the data object into JSON form, and then writes that to a local JSON file "itinerary.json" that is used to show the end user the trip. [TODO] Give credit for GSON
    public void writeJSON(ArrayList<String> compDistances /*[TODO] Replace with actual data structure*/) throws IOException {
       Gson gsonObj = new Gson();
 
       FileWriter writer = new FileWriter("itinerary.json");
       gsonObj.toJson(computedDistances, writer);
+      writer.flush();
+      writer.close();
+
    }
 }
