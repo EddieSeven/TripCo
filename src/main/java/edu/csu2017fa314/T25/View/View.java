385d0b345188f4ed6babaa43e9882547015ae50b
Michael <mlyonsru@gmail.com>
2017-09-06 16:39:18 -0600

    Added Will's data structure to view

--- src/main/java/edu/csu2017fa314/T25/View/View.java
+++ src/main/java/edu/csu2017fa314/T25/View/View.java
@@ -1,14 +1,13 @@
 package edu.csu2017fa314.T25.View;
 
 import com.google.gson.Gson;
+import edu.csu2017fa314.T25.Model.TripLeg;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class View
-{
-   ArrayList<String> computedDistances = new ArrayList<String>(); /*[TODO] Replace with actual data structure*/
+public class View {
 
    private int totalDistance;
 
@@ -25,7 +24,7 @@ public class View
 
    // Takes in a data structure consisting of a collection of data with three fields: id for one point, id for another point, and the distance between the two.
    // Uses the gson class to turn the data object into JSON form, and then writes that to a local JSON file "itinerary.json" that is used to show the end user the trip. [TODO] Give credit for GSON
-   public void writeJSON(ArrayList<String> compDistances /*[TODO] Replace with actual data structure*/) throws IOException {
+   public void writeJSON(ArrayList<TripLeg> computedDistances) throws IOException {
       Gson gsonObj = new Gson();
 
       FileWriter writer = new FileWriter("itinerary.json");
