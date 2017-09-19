7cd9880474d7939395206fd16e917ad004642a7e
Michael <mlyonsru@gmail.com>
2017-09-19 14:21:29 -0600

    added skeleton of nearestNeighbor class

---
+++ src/main/java/edu/csu2017fa314/T25/Model/NearestNeighbor.java
@@ -0,0 +1,30 @@
+package edu.csu2017fa314.T25.Model;
+
+import java.util.ArrayList;
+
+
+
+public class NearestNeighbor {
+    ArrayList<Node> unvisited = new ArrayList<Node>();
+
+    /**
+     * INPUT: current node
+     * OUTPUT: nearest node
+     * */
+    private Node computeNearestNeighbor(Node current){
+        return null;
+    }
+
+    /**
+    * INPUT: data structure containing all the stops in the trip
+    * OUTPUT: either that same data structure or a path through all of the nodes
+    * */
+    private ArrayList<Node> computePath(){
+        return null;
+    }
+
+}
+
+class Node {
+
+}
