631b20f3cce8f357eab6e19d719993194c3e65ab
Michael <mlyonsru@gmail.com>
2017-10-12 20:24:59 -0600

    added Result container that consists of a 2d string array, which should be pretty light weight

--- src/test/java/edu/csu2017fa314/T25/Model/TestDatabaseDriver.java
+++ src/test/java/edu/csu2017fa314/T25/Model/TestDatabaseDriver.java
@@ -3,7 +3,6 @@ package edu.csu2017fa314.T25.Model;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TestDatabaseDriver {
@@ -11,19 +10,11 @@ public class TestDatabaseDriver {
 
     @Before
     public void setup() throws ClassNotFoundException {
-       // test = new DatabaseDriver("mlyonsru", "830721900"); // won't work with travis
+        test = new DatabaseDriver("mlyonsru", "830721900"); // won't work with travis
     }
 
     @Test
     public void queryTest(){
-     /*
-        try {
-            ResultSet result = test.query("heli");
-            test.printResults(result);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    */
+        Result result = test.query("heli");
     }
 }
