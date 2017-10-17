fef13d76d04444e2926b3ca85497aeb5a7ebbe9f
willyingling <willyingling@gmail.com>
2017-10-17 14:47:04 -0600

    Changed to point arraylist>

--- src/main/java/edu/csu2017fa314/T25/Model/Result.java
+++ src/main/java/edu/csu2017fa314/T25/Model/Result.java
@@ -1,11 +1,13 @@
 package edu.csu2017fa314.T25.Model;
 
+import java.util.ArrayList;
+
 class Result {
-    String result[][];
+	ArrayList<Point> points;
     int total;
 
-    public Result(String result[][], int total) {
-        this.result = result;
-        this.total = total;
-    }
+	public Result(ArrayList<Point> pts) {
+		points = pts;
+		total = pts.size();
+	}
 }
