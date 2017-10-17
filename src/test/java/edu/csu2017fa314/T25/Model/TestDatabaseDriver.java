1a70789e195009cff24aa222ce1c534a9c93027c
Michael <mlyonsru@gmail.com>
2017-10-17 15:12:02 -0600

    changed SQL connector parameters for tests

--- src/test/java/edu/csu2017fa314/T25/Model/TestDatabaseDriver.java
+++ src/test/java/edu/csu2017fa314/T25/Model/TestDatabaseDriver.java
@@ -11,25 +11,25 @@ public class TestDatabaseDriver {
 
     @Before
     public void setup() throws ClassNotFoundException {
-        //   test = new DatabaseDriver("mlyonsru", "830721900", "jdbc:mysql://localhost:8080/cs314?useLegacyDatetimeCode=false&serverTimezone=UTC"); // todo Doesn't work with travis
+           test = new DatabaseDriver("travis", "", "jdbc:mysql://TripCoTestDB"); // todo Doesn't work with travis
     }
 
     @Test
     public void queryTest() {
         // Test 1 - Swedish Hospitals
-//        Result result = test.query("swe");
-//        assertEquals(true,         result.result[0][0].equals("0CD9"));
-//        assertEquals(true,         result.result[1][0].equals("15CO"));
+        Result result = test.query("swe");
+        assertEquals(true,         result.result[0][0].equals("0CD9"));
+        assertEquals(true,         result.result[1][0].equals("15CO"));
 
         // Test 2 - Limit
-//        result = test.query("a");
-//        assertEquals(true, result.result[0][0].equals("KBJC"));
-//        assertEquals(true, result.result[49][0].equals("1CO2"));
+        result = test.query("a");
+        assertEquals(true, result.result[0][0].equals("KBJC"));
+        assertEquals(true, result.result[49][0].equals("1CO2"));
 
         // Test 3 - Denver
-//        result = test.query("denver");
-//        assertEquals(true, result.result[0][0].equals("KBJC"));
-//        assertEquals(true, result.result[7][0].equals("9CO0"));
-//        assertEquals(true, result.result[25][0].equals("US-0073"));
+        result = test.query("denver");
+        assertEquals(true, result.result[0][0].equals("KBJC"));
+        assertEquals(true, result.result[7][0].equals("9CO0"));
+        assertEquals(true, result.result[25][0].equals("US-0073"));
     }
 }
