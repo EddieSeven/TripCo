2ed881c790b3aad07f2fc367b9f10631c6de28ed
Michael <mlyonsru@gmail.com>
2017-12-03 19:02:16 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 cd web
 npm run build
 cp index.html public/
-cp -r resources/ public/
+cp styles.css public/
+cp -r resources/images/ public/
 
 ./ss "java -cp ../target/T25-4.0-jar-with-dependencies.jar edu.csu2017fa314.T25.TripCo" "npm start"
