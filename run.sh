f29c7b2d7d7890459ef1395688b675fa4286a056
Michael <mlyonsru@gmail.com>
2017-12-07 18:30:27 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ cp index.html public/
 cp styles.css public/
 cp -r resources/images/ public/
 
-./ss "java -cp ../target/T25-4.0-jar-with-dependencies.jar edu.csu2017fa314.T25.TripCo" "npm start"
+./ss "java -cp ../target/T25-5.0-jar-with-dependencies.jar edu.csu2017fa314.T25.TripCo" "npm start"
